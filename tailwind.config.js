6fb0e7995262432d31f38bc34d1ec0271a29c140
BendikOA <bendik940@gmail.com>
2023-02-23 23:51:28 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+      jsm: '375px',
+      jmd: '834',
+      jlg: '1512',
+    },
+    colors: {
+      'brun': '#1D1006',
+      'gul': '#F5B845',
+      'hvit': '#E2D2BF',
+    },
+    extend: {
+      fontFamily: {
+        youngs: 'YoungSerif, Normal',
+        sans: ['Work Sans', 'sans-serif'],
+
+      }
+    },
+  },
+  plugins: [],
+}
