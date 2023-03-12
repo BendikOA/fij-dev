9abf2e8142e80c31706917e1a6730e7d871119ca
BendikOA <bendik940@gmail.com>
2023-03-12 19:10:49 +0100

    44

--- Components/Bestilling.jsx
+++ Components/Bestilling.jsx
@@ -40,9 +40,7 @@ const Bestilling = () => {
                             <p className=' mt-[13px]'>
                                 Ved bestilling, vennligst oppgi navn, adresse, betalingsmåte (Vipps, bankoverføring) om faktura ønskes tilsendt og om boka ønskes tilsendt med Posten eller avhentes i Oslo eller på Ås etter avtale.
                             </p>
-                            <button className='border-hvit border-2 rounded-[4px] mt-7 mb-8 md:pb-0 font-youngs'>
-                                <p className='text-hvit font-youngs font-[16px] pt-[6px] pb-[6px] px-[10px]'>Les forhåndsvisning</p>
-                            </button>
+                        
 
                         </div>
                     </div>
---
+++ Components/Forknapp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Forknapp = () => {
+  return (
+    <button className='border-hvit border-2 rounded-[4px] mt-7 mb-8 md:pb-0 font-youngs'>
+    <p className='text-hvit font-youngs font-[16px] pt-[6px] pb-[6px] px-[10px]'>Les forhåndsvisning</p>
+</button>  )
+}
+
+export default Forknapp
