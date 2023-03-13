e2840462c88e40d663c2c43ef7636d19044f96f3
BendikOA <bendik940@gmail.com>
2023-03-13 14:33:05 +0100

    53

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Fingern i jorda</title>
-        <meta name="description" content="Finger'n i jorda - Hjem 2" />
+        <title>Finger'n i jorda</title>
+        <meta name="description" content="hjemmesiden da lissom" />
         <meta property="og:image" content="/fijimg.webp" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
