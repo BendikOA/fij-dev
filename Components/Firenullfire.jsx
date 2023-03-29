4ad4d8f2aa65fd3a4cddbdfb31e263e7e0a39c04
BendikOA <bendik940@gmail.com>
2023-03-29 22:49:28 +0200

    70

--- Components/Firenullfire.jsx
+++ Components/Firenullfire.jsx
@@ -20,13 +20,14 @@ const firenullfire = () => {
   return (
     <div className='w-full min-h-screen'>
       <div className='mx-[1.25rem] md:mx-[3.75rem] lg:mx-[9.3rem] pt-[4.1rem] md:pt-[11.3rem] lg:pt-[16rem] jlg:pt-[18.75rem] py-auto'>
-        <div className='hidden md:flex  justify-center text-hvit'>
+        <div className='hidden md:flex justify-center text-hvit'>
 
           <img
+          className='w-[8rem]'
             src="/erroricon.webp"
-            width="15%" height="100%"
+            width="100%" height="100%"
             alt="Jordnaert Press" />        </div>
-        <div className='justify-center  place-items-center  grid md:grid-flow-col md: gap-36 lg:gap-48 xl:gap-96'>
+        <div className='justify-center   grid md:grid-flow-col md: gap-36 lg:gap-48 xl:gap-96'>
 
           <div className='max-w-lg mx-[30px] text-center'>
             <h1 className='font-youngs text-[7.5rem] text-hvit '>404</h1>
@@ -39,13 +40,14 @@ const firenullfire = () => {
           </div>
           <div className=' md:hidden  justify-center text-hvit'>
 
-            <img
+            <img className='justify-center mx-auto'
               src="/erroricon.webp"
-              width="100%" height="100%"
-              alt="Jordnaert Press" />        </div>
-          <div>
-            <div className='pb-24 md:py-24 '>
-              <ul className='text-center md:text-right text-[1.25rem] text-hvit font-youngs '>
+              width="50%" height="100%"
+              alt="Jordnaert Press" />       
+               </div>
+          <div className='pb-24 md:pt-20'>
+            <div>
+              <ul className='text-center md:text-right text-[1.25rem] text-hvit font-youngs mt-8'>
                 <li className={` hover:text-gul ${isActive('/')}`}>
                   <Link href="/">
                     Finger'n i jorda
